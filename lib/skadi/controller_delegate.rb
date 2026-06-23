cc62ee94ba9cd33b36537cc20ea0caae52f782b7
Simon Jarrett <simon.jarrett@nesta.org.uk>
2026-06-23 12:31:42 +0100

    Fix copy & paste typo in controller delegate persist error

--- lib/skadi/controller_delegate.rb
+++ lib/skadi/controller_delegate.rb
@@ -82,7 +82,7 @@ module Skadi
         Event.insert_all(@events)
       end
     rescue ActiveRecord::ActiveRecordError => e
-      Rails.logger.error("Skadi: failed to persist analytics for #{controller.controller_name}##{controller.action_name} (visit: #{@visit.try(:id).inspect}, view: #{@visit.try(:id).inspect}, events: #{@events.count}, demographics: #{@demographics.count}): #{e.class}, #{e.message}; Line: #{e.backtrace&.first}")
+      Rails.logger.error("Skadi: failed to persist analytics for #{controller.controller_name}##{controller.action_name} (visit: #{@visit.try(:id).inspect}, view: #{@view.try(:id).inspect}, events: #{@events.count}, demographics: #{@demographics.count}): #{e.class}, #{e.message}; Line: #{e.backtrace&.first}")
 
       raise if Rails.env.local?
     end
